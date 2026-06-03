57e8f74eb1b044a93f10a953576c4c95b1003ddf
Thalion1 <gustav.kodehode@gmail.com>
2026-06-03 14:52:45 +0200

    color change based on theam, input and submit added

--- src/DisplayData.jsx
+++ src/DisplayData.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Chart, Title, Legend } from "@highcharts/react";
+import { Chart, Title, Legend, XAxis, YAxis } from "@highcharts/react";
 import { SplineSeries } from "@highcharts/react/series/Spline";
 import { ColumnSeries } from "@highcharts/react/series/Column";
 export function SplineChart(props) {
     const series = props.data || [];
 
     return (
-        <Chart>
-            <Title>{props.title}</Title>
+        <Chart backgroundColor={props.color.background}>
+            <Title>{`<span style="color:${props.color.text}">${props.title}</span>`}</Title>
+            <XAxis labels={{ style: { color: props.color.text } }} title={{ style: { color: props.color.text } }}/>
+            <YAxis labels={{ style: { color: props.color.text } }} title={{ style: { color: props.color.text } }}/>
             {series.map((s, i) => (
-                <SplineSeries key={s.name ?? i} data={s.data} name={s.name} />
+                <SplineSeries key={s.name ?? i} data={s.data} name={`<span style="color:${s.color ?? props.color.text}">${s.name}</span>`} />
             ))}
         </Chart>
     );
@@ -17,10 +19,10 @@ export function SplineChart(props) {
 
 export function ColumnChart(props) {
     return (
-        <Chart>
-          <Title>{props.title}</Title>
+        <Chart backgroundColor={props.color.background}>
+          <Title>{`<span style="color:${props.color.text}">${props.title}</span>`}</Title>
           {/* <Legend>{"{index}: {name}"}</Legend> */}
-          <ColumnSeries data={props.data} name="Column series" color="red" />
+          <ColumnSeries data={props.data} name={`<span style="color:${props.color.text}">column chart</span>`} color="red" />
         </Chart>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,40 +4,24 @@ import "./index.css";
 import { SplineChart, ColumnChart } from "./DisplayData.jsx";
 import { getWeather, getWhere } from "./GetApiData.jsx";
 
-const data = [
-	{
-        data: [
-            3,
-            4,
-            {
-                y: 26.4,
-                marker: {
-                    symbol: "url(https://www.highcharts.com/samples/graphics/sun.png)"
-                },
-                accessibility: {
-                    description: "Sunny symbol, this is the warmest point in the " + "chart."
-                }
-            },
-            5,
-            2
-        ],
-        name: "yr"
-    },{
-        data: [2,
-            3,
-            2,
-            4,
-            1
-        ],
-        name: "storm"
-    }
-];
+const data = await getWeather({ lat: 59.9133301, lon: 10.7389701 });
+let backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--bg').trim();
+let textColor = getComputedStyle(document.documentElement).getPropertyValue('--text').trim();
+
+console.log(document.getElementById('user-submit'));
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<SplineChart title="temp for the day" data={data} />
-		<ColumnChart title="rain for the day" data={[3, 4, 1, 5, 2]} />
+    <StrictMode>
+        <div>
+            <input type="text" id="user-input" />
+            <input type="submit" value="submit" id="user-submit" />
+        </div>
+        <div>
+		    <SplineChart title="temp for the day" data={data} color={{background: backgroundColor, text: textColor}} />
+		    <ColumnChart title="rain for the day" data={[3, 4, 1, 5, 2]} color={{background: backgroundColor, text: textColor}} />
+            <SplineChart title="wind for the day" data={data} color={{background: backgroundColor, text: textColor}} />
+        </div>
 	</StrictMode>,
 );
 
-getWeather({ lat: 59.9133301, lon: 10.7389701 });
+console.log(document.getElementById('user-submit'));
